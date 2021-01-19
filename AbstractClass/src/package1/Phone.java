3a2c3a524abd7eddd24838f701299842bf7b8dea
CalvinFestus <60812127+CalvinFestus@users.noreply.github.com>
2021-01-19 09:43:52 +0530

    Add files via upload

---
+++ AbstractClass/src/package1/Phone.java
@@ -0,0 +1,14 @@
+package package1;
+
+public  abstract class Phone {
+	
+	int num = 1;
+	int sum;
+	
+	public void calling() {
+		System.out.println("This Method is for Calling");
+	}
+	
+	public abstract void simCard();
+	
+}
---
+++ AbstractClass/src/package1/PhoneRules.java
@@ -0,0 +1,9 @@
+package package1;
+
+public  interface PhoneRules {
+	
+	int num = 10;
+	
+	public void imeiNumber();
+
+}
